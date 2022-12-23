4932a7ff4057034851a5ab52fc629f8d62714c11
Wouter Massa <woutermassa@hotmail.com>
2022-12-22 20:45:12 -0800

    even simpler repro

---
+++ main.js
@@ -0,0 +1,28 @@
+import './style.css';
+import javascriptLogo from './javascript.svg';
+import { setupCounter } from './counter.js';
+
+import flow from 'lodash/flow';
+import split from 'lodash/fp/split';
+
+export const confNumbers = flow(split(/[;,\s]+/))('e');
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector('#counter'));
